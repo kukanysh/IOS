07bc69e889ca269d179a4cb98b82bb230067efb8
kukanysh <260505kuka@gmail.com>
2023-08-28 20:18:23 +0600

    ups

---
+++ OnboardingScreenView.swift
@@ -0,0 +1,97 @@
+//
+//  OnboardingScreenView.swift
+//  Kettik!
+//
+//  Created by Куаныш Спандияр on 28.08.2023.
+//
+
+import SwiftUI
+
+struct OnboardingScreenView: View {
+    
+    @State private var pageIndex = 0
+    private let pages: [Page] = Page.samplePages
+    private let dotAppearance = UIPageControl.appearance()
+    
+    @Binding var isOnboardingCompleted: Bool
+    
+    var body: some View {
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    Spacer()
+                    PageView(page: page)
+                    Spacer()
+                    
+                    if page == pages.last {
+                        Button(action: {
+                            goToZero()
+                        }, label: {
+                            Image(systemName: "arrow.right")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, height: 39)
+                                .foregroundColor(.black)
+                                .background {
+                                    Circle()
+                                        .fill(Color("tabBar"))
+                                        .frame(width: 80, height: 80)
+                                    
+                                }
+                        }).position(x: 350, y: 350)
+                        
+                            
+                
+                    } else {
+                        Button(action: {
+                            incrementPage()
+                        }, label: {
+                            Image(systemName: "arrow.right")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, height: 39)
+                                .foregroundColor(.black)
+                                .background {
+                                    Circle()
+                                        .fill(Color("tabBar"))
+                                        .frame(width: 80, height: 80)
+                                    
+                                }
+                        }).position(x: 350, y: 350)
+                        
+                        
+                        Spacer()
+                    }
+                    
+             
+                    
+                }.tag(page.tag)
+                    .edgesIgnoringSafeArea(.top)
+                
+            }
+        }
+        .animation(.easeInOut, value: pageIndex)
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        .onAppear {
+            dotAppearance.currentPageIndicatorTintColor = .black
+            dotAppearance.pageIndicatorTintColor = .gray
+        }
+        .edgesIgnoringSafeArea(.all)
+        
+    }
+    
+    func incrementPage() {
+        pageIndex += 1
+    }
+    
+    func goToZero() {
+        isOnboardingCompleted = true
+    }
+}
+
+struct OnboardingScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingScreenView(isOnboardingCompleted: .constant(false))
+    }
+}
