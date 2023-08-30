58cfbf06abb2fb69c3f41d4ce6c11721ff283881
kukanysh <260505kuka@gmail.com>
2023-08-30 23:33:35 +0600

    updated onboarding screen

--- ALA-Mountains/PageModel.swift
+++ ALA-Mountains/PageModel.swift
@@ -15,11 +15,11 @@ struct Page: Identifiable, Equatable {
     var imageUrl: String
     var tag: Int
     
-    static var samplePage = Page(name: "Explore New Places", description: "Discover the most precious places in Kazakhstan", imageUrl: "Shymbulak", tag: 0)
+    static var samplePage = Page(name: "Explore New Places", description: "Discover the most precious places in Kazakhstan", imageUrl: "first", tag: 0)
     
     static var samplePages: [Page] = [
-        Page(name: "Explore New Places", description: "Discover the most precious places in Kazakhstan", imageUrl: "Kolsay2", tag: 1),
-        Page(name: "Welcome to Kettik!", description: "Discover the most precious places in Kazakhstan", imageUrl: "Aksu", tag: 2),
-        Page(name: "You are all set", description: "Let's find your paradise", imageUrl: "Almatinskiy_pik", tag: 3),
+        Page(name: "Welcome to Kettik!", description: "Discover the most precious places in Kazakhstan. Choose your destination and plan your trip.", imageUrl: "first", tag: 1),
+        Page(name: "Explore New Places", description: "Find fabulous places for your inner peace", imageUrl: "second", tag: 2),
+        Page(name: "You are ready to go!", description: "Let's find your paradise in Kazakhstan", imageUrl: "third", tag: 3),
     ]
 }
--- PageView.swift
+++ PageView.swift
@@ -13,37 +13,43 @@ struct PageView: View {
     
     var body: some View {
         ZStack {
-            Image(page.imageUrl)
-                .resizable()
-                .frame(height: 450)
-                .scaledToFit()
-                .edgesIgnoringSafeArea(.top)
-                .position(x: 215, y: 250)
-            
-            RoundedRectangle(cornerRadius: 55)
-                .frame(width: UIScreen.main.bounds.width, height: 700)
-                .foregroundColor(.white)
-                .position(x: 215, y: 750)
             
             Text(page.name)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .fontDesign(.rounded)
+                .fontDesign(.monospaced)
                 .foregroundColor(.black)
                 .multilineTextAlignment(.leading)
                 .frame(width: 300, alignment: .leading)
-                .position(x: 193, y: 500)
+                .position(x: 175, y: 150)
                 .padding(.trailing)
             
+            
             Text(page.description)
-                .font(.title)
-                .fontWeight(.medium)
+                .font(.title2)
+                .fontWeight(.light)
                 .fontDesign(.rounded)
+                .fontDesign(.monospaced)
                 .foregroundColor(.black)
                 .lineSpacing(7)
+                .opacity(0.5)
                 .multilineTextAlignment(.leading)
                 .frame(width: 400, alignment: .leading)
-                .position(x: 243, y: 600)
+                .position(x: 225, y: 270)
+            
+            Image(page.imageUrl)
+                .resizable()
+                .frame(height: 450)
+                .scaledToFit()
+                .edgesIgnoringSafeArea(.top)
+                .position(x: 215, y: 580)
+            
+            
+            
+            
+            
+            
             
             
         }.edgesIgnoringSafeArea(.all)
